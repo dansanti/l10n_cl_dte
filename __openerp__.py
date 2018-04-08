2853f64698e0c9bf1935f32c71fed19d382aaf31
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-04-08 00:57:37 -0300

    [FIX]Propiedad mal nombrada
    close #16

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
 {
     "name": """Chile - Web Services de Documentos Tributarios Electrónicos\
     """,
-    'version': '9.0.8.1.1',
+    'version': '9.0.8.1.2',
     'category': 'Localization/Chile',
     'sequence': 12,
     'author':  'Daniel Santibáñez Polanco, BMyA SA - Blanco Martín & Asociados, Odoo Chile',
